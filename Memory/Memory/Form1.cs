43394eeb3d7d4b16d139ab4bd45b50c3e169bffa
BlueFireKnightYT <vanmeerveldsGames@outlook.com>
2026-02-03 13:11:30 +0100

    Memory game

---
+++ Memory/Memory/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Memory
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<string> icons = new List<string>() { "b", "b", "e", "e", "l", "l", "j", "j", "p", "p", "f", "f", "+", "+", "*", "*" };
+
+        Label firstClicked, secondClicked;
+        Timer hideTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIcons();
+
+            hideTimer = new Timer();
+            hideTimer.Interval = 750;
+            hideTimer.Tick += HideTimer_Tick;
+        }
+
+        private void AssignIcons()
+        {
+            Label label;
+            int randomNumber;
+
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Label)
+                {
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                }
+                else continue;
+
+                randomNumber = random.Next(0, icons.Count);
+                label.Text = icons[randomNumber];
+                icons.RemoveAt(randomNumber);
+
+                
+                label.ForeColor = Color.SkyBlue;
+            }
+        }
+
+        private void ChangeText(object sender, EventArgs e)
+        {
+            if (hideTimer.Enabled)
+                return;
+
+            Label clickedLabel = sender as Label;
+            if (clickedLabel == null)
+                return;
+
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+
+            if (firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                firstClicked.ForeColor = Color.Black;
+                return;
+            }
+
+            if (clickedLabel == firstClicked)
+                return;
+
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+
+            hideTimer.Start();
+        }
+
+        private void HideTimer_Tick(object sender, EventArgs e)
+        {
+            hideTimer.Stop();
+
+            if (firstClicked != null && secondClicked != null)
+            {
+                if (firstClicked.Text != secondClicked.Text)
+                {
+                    firstClicked.ForeColor = firstClicked.BackColor;
+                    secondClicked.ForeColor = secondClicked.BackColor;
+                }
+
+                firstClicked = null;
+                secondClicked = null;
+            }
+        }
+    }
+}
--- Memory/Memory/Properties/AssemblyInfo.cs
+++ Memory/Memory/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApp1")]
+[assembly: AssemblyTitle("Memory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyProduct("Memory")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59983d36-2c60-40a1-8f12-f98342dabdfa")]
+[assembly: Guid("e2482516-0206-4afc-8204-a3a7b0548860")]
 
 // Version information for an assembly consists of the following four values:
 //
